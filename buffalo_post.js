d8e81fccb21d2e905cee93ecd1d1eb04c1816122
Sam Hains <sdh@eml.cc>
2017-12-07 15:14:19 -0500

    dfadfsa

--- buffalo_post.js
+++ buffalo_post.js
@@ -18,6 +18,6 @@ $( document ).ready(function() {
       body = posts[i]
       html_str = `<tr> <td align="center" valign="top" width="150" bgcolor="" style="word-wrap: break-word"> <div class="whitetext12"> Buffalo </div> <br> <br> <img src="http://www.ci.buffalo.ny.us/files/1_2_1/public%20art%20website/Images/David.jpg" class="comment-img"  border="0"> <div style="width:80px;height:20px;" id="UserDataNode1" class="DataPoint=OnlineNow;UserID=61789441;"></div> <br> <br> </td> <td bgcolor="white" align="left" valign="top" width="260" style="word-wrap: break-word"> <span class="blacktext10"> 3/11/2006 2:03 AM </span> <br> <br> ${body}</td> </tr> // do_something_with(data)`
       $("#comment-body").append(html_str)
-    }, 10000)
+    }, 40000)
   }, 'text');
 });
